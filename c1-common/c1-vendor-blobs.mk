dccf53a88f541ae7755fce7019694323ca22415a
Kolja Dummann <k.dummann@gmail.com>
2011-06-19 10:02:08 +0200

    gps libs
    Change-Id: I4c0feee6379b6f02384bad37c24a6741038a8dd8

--- c1-common/c1-vendor-blobs.mk
+++ c1-common/c1-vendor-blobs.mk
@@ -80,7 +80,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     vendor/samsung/c1-common/proprietary/hw/copybit.GT-I9100.so:system/lib/hw/copybit.GT-I9100.so \
-    vendor/samsung/c1-common/proprietary/hw/gps.GT-I9100.so:system/lib/hw/gps.GT-I9100.so \
+    vendor/samsung/c1-common/proprietary/hw/gps.GT-I9100.so:system/lib/hw/vendor-gps.GT-I9100.so \
     vendor/samsung/c1-common/proprietary/hw/gralloc.default.so:system/lib/hw/gralloc.default.so \
     vendor/samsung/c1-common/proprietary/hw/gralloc.GT-I9100.so:system/lib/hw/gralloc.GT-I9100.so \
     vendor/samsung/c1-common/proprietary/hw/lights.GT-I9100.so:system/lib/hw/lights.GT-I9100.so \
