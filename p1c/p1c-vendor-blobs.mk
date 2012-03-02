ccf235db2585b54dee4942b9703deda63f388a30
Chirayu Desai <chirayudesai1@gmail.com>
2012-03-02 16:48:13 +0530

    typo(s)

--- p1c/p1c-vendor-blobs.mk
+++ p1c/p1c-vendor-blobs.mk
@@ -3,7 +3,7 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
+#t
 #      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
@@ -14,13 +14,13 @@
 
 # Prebuilt libraries that are needed to build open-source libraries
 PRODUCT_COPY_FILES := \
-    vendor/samsung/p1/proprietary/libril.so:obj/lib/libril.so \
-    vendor/samsung/p1/proprietary/libsecril-client.so:obj/lib/libsecril-client.so
+    vendor/samsung/p1c/proprietary/libril.so:obj/lib/libril.so \
+    vendor/samsung/p1c/proprietary/libsecril-client.so:obj/lib/libsecril-client.so
 
 # RIL (CDMA)
 PRODUCT_COPY_FILES += \
-    vendor/samsung/p1/proprietary/libril.so:system/lib/libril.so \
-    vendor/samsung/p1/proprietary/libsecril-client.so:system/lib/libsecril-client.so \
-    vendor/samsung/p1/proprietary/libsec-ril40.so:system/lib/libsec-ril40.so \
-    vendor/samsung/p1/proprietary/rild:system/bin/rild \
-    vendor/samsung/p1/proprietary/pppd_runner:system/bin/pppd_runner
+    vendor/samsung/p1c/proprietary/libril.so:system/lib/libril.so \
+    vendor/samsung/p1c/proprietary/libsecril-client.so:system/lib/libsecril-client.so \
+    vendor/samsung/p1c/proprietary/libsec-ril40.so:system/lib/libsec-ril40.so \
+    vendor/samsung/p1c/proprietary/rild:system/bin/rild \
+    vendor/samsung/p1c/proprietary/pppd_runner:system/bin/pppd_runner
