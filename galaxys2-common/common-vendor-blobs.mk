f9a2055b3c7e79c5cfb2f1c6afe6bc714417eb9b
codeworkx <daniel.hillenbrand@codeworkx.de>
2012-03-24 00:44:33 +0100

    galaxys2: libfimc to obj

--- galaxys2-common/common-vendor-blobs.mk
+++ galaxys2-common/common-vendor-blobs.mk
@@ -14,7 +14,8 @@
 
 # Prebuilt libraries that are needed to build open-source libraries
 PRODUCT_COPY_FILES += \
-    vendor/samsung/galaxys2-common/proprietary/hwcomposer/lib/libTVOut.so:obj/lib/libTVOut.so 
+    vendor/samsung/galaxys2-common/proprietary/hwcomposer/lib/libTVOut.so:obj/lib/libTVOut.so \
+    vendor/samsung/galaxys2-common/proprietary/hwcomposer/lib/libfimc.so:obj/lib/libfimc.so
 
 # EGL
 PRODUCT_COPY_FILES += \
