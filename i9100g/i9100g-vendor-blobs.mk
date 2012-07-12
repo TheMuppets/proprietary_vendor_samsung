c03863723965d8b5e03347aa9e2202adcd8b42b7
Daniel Hillenbrand <daniel.hillenbrand@codeworkx.de>
2012-07-12 22:15:23 +0200

    i9100g

--- i9100g/i9100g-vendor-blobs.mk
+++ i9100g/i9100g-vendor-blobs.mk
@@ -118,7 +118,7 @@ PRODUCT_COPY_FILES += \
 #    $(LOCAL_PATH)/proprietary/system/lib/libtvout_jni.so:system/lib/libtvout_jni.so \
 #    $(LOCAL_PATH)/proprietary/system/lib/libtvoutservice.so:system/lib/libtvoutservice.so
 
-PRODUCT_COPY_FILES += \
+#PRODUCT_COPY_FILES += \
 #    $(LOCAL_PATH)/proprietary/system/lib/hw/camera.t1.so:system/lib/hw/vendor-camera.t1.so \
 #    $(LOCAL_PATH)/proprietary/system/lib/libdomx.so:system/lib/libdomx.so \
 #    $(LOCAL_PATH)/proprietary/system/lib/libI420colorconvert.so:system/lib/libI420colorconvert.so \
@@ -135,9 +135,9 @@ PRODUCT_COPY_FILES += \
 #    $(LOCAL_PATH)/proprietary/system/lib/libtiutils.so:system/lib/libtiutils.so
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/proprietary/system/lib/hw/gps.omap4.so:system/lib/hw/gps.omap4.so
 #    $(LOCAL_PATH)/proprietary/system/lib/hw/audio.primary.omap4.so:system/lib/hw/audio.primary.omap4.so \
 #    $(LOCAL_PATH)/proprietary/system/lib/hw/audio_policy.omap4.so:system/lib/hw/audio_policy.omap4.so \
-    $(LOCAL_PATH)/proprietary/system/lib/hw/gps.omap4.so:system/lib/hw/gps.omap4.so \
 #    $(LOCAL_PATH)/proprietary/system/lib/hw/sensors.omap4.so:system/lib/hw/sensors.omap4.so
 
 PRODUCT_COPY_FILES += \
