3682a9ea4228e43bba6d8648011a95514e5263ec
Daniel Hillenbrand <daniel.hillenbrand@codeworkx.de>
2012-07-18 22:12:01 +0200

    p31xx: remove audio blobs

--- p51xx/p51xx-vendor-blobs.mk
+++ p51xx/p51xx-vendor-blobs.mk
@@ -31,7 +31,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/common/system/lib/hw/camera.omap4.so:system/lib/hw/camera.omap4.so \
     $(LOCAL_PATH)/common/system/lib/hw/gps.omap4.so:system/lib/hw/gps.omap4.so \
     $(LOCAL_PATH)/common/system/lib/hw/sensors.omap4.so:system/lib/hw/sensors.omap4.so \
-    $(LOCAL_PATH)/common/system/lib/libsecnativefeature.so:system/lib/libsecnativefeature.so \
     $(LOCAL_PATH)/common/system/lib/libtiutils.so:system/lib/libtiutils.so
 
 PRODUCT_COPY_FILES += \
