2722ce579ed54bed985ea32c55f1d30190375cb4
Daniel Hillenbrand <daniel.hillenbrand@codeworkx.de>
2012-07-18 23:28:06 +0200

    p31xx/p51xx: hwcomposer

--- p31xx/p31xx-vendor-blobs.mk
+++ p31xx/p31xx-vendor-blobs.mk
@@ -30,6 +30,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/common/system/lib/hw/camera.omap4.so:system/lib/hw/camera.omap4.so \
     $(LOCAL_PATH)/common/system/lib/hw/gps.omap4.so:system/lib/hw/gps.omap4.so \
+    $(LOCAL_PATH)/common/system/lib/hw/hwcomposer.piranha.so:system/lib/hw/hwcomposer.piranha.so \
     $(LOCAL_PATH)/common/system/lib/hw/sensors.omap4.so:system/lib/hw/sensors.omap4.so \
     $(LOCAL_PATH)/common/system/lib/libtiutils.so:system/lib/libtiutils.so
 
