7cbed197f287406c748c1f1e384d371b3dfb6c56
Robert Burns <burnsra@gmail.com>
2012-08-01 23:56:30 -0400

    Nexus S/4G binaries for Android 4.1.1 (JRO03H)

--- crespo4g/proprietary/Android.mk
+++ crespo4g/proprietary/Android.mk
@@ -14,8 +14,6 @@
 
 LOCAL_PATH:=$(call my-dir)
 
-ifeq ($(TARGET_DEVICE),crespo4g)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := WiMAXSettings
 LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
@@ -51,5 +49,3 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 include $(BUILD_PREBUILT)
-
-endif
