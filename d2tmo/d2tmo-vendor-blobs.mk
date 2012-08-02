5af51f78b2f12f84118e297b62888b23b5899d4d
Scott Warner <Tortel1210@gmail.com>
2012-08-01 22:23:42 -0400

    Update d2 blobs

--- d2tmo/d2tmo-vendor-blobs.mk
+++ d2tmo/d2tmo-vendor-blobs.mk
@@ -49,6 +49,7 @@ PRODUCT_COPY_FILES += \
         vendor/samsung/d2tmo/proprietary/lib/lib_Samsung_Resampler.so:system/lib/lib_Samsung_Resampler.so \
         vendor/samsung/d2tmo/proprietary/lib/lib_SamsungRec_V01005.so:system/lib/lib_SamsungRec_V01005.so \
         vendor/samsung/d2tmo/proprietary/lib/libcontrolcsc.so:system/lib/libcontrolcsc.so \
+        vendor/samsung/d2tmo/proprietary/lib/libmm-color-convertor.so:system/lib/libmm-color-convertor.so \
         vendor/samsung/d2tmo/proprietary/lib/libmmparser.so:system/lib/libmmparser.so \
         vendor/samsung/d2tmo/proprietary/lib/libmmosal.so:system/lib/libmmosal.so \
         vendor/samsung/d2tmo/proprietary/lib/libDivxDrm.so:system/lib/libDivxDrm.so \
--- d2vzw/d2vzw-vendor-blobs.mk
+++ d2vzw/d2vzw-vendor-blobs.mk
@@ -49,6 +49,7 @@ PRODUCT_COPY_FILES += \
         vendor/samsung/d2vzw/proprietary/lib/lib_Samsung_Resampler.so:system/lib/lib_Samsung_Resampler.so \
         vendor/samsung/d2vzw/proprietary/lib/lib_SamsungRec_V01005.so:system/lib/lib_SamsungRec_V01005.so \
         vendor/samsung/d2vzw/proprietary/lib/libcontrolcsc.so:system/lib/libcontrolcsc.so \
+        vendor/samsung/d2vzw/proprietary/lib/libmm-color-convertor.so:system/lib/libmm-color-convertor.so \
         vendor/samsung/d2vzw/proprietary/lib/libmmparser.so:system/lib/libmmparser.so \
         vendor/samsung/d2vzw/proprietary/lib/libmmosal.so:system/lib/libmmosal.so \
         vendor/samsung/d2vzw/proprietary/lib/libDivxDrm.so:system/lib/libDivxDrm.so \
