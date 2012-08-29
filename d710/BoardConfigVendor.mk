b3f7f25add1f16eddefed06dca30c74d5df48531
sbrissen <sbrissen@hotmail.com>
2012-08-29 16:21:45 -0400

    d710: fix headers

--- d710/BoardConfigVendor.mk
+++ d710/BoardConfigVendor.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2010 The Android Open Source Project
+# Copyright (C) 2012 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#BOARD_GPS_LIBRARIES := libgps
-USE_CAMERA_STUB := false
-BOARD_USES_GENERIC_AUDIO := false
-BOARD_USES_LIBSECRIL_STUB := false
