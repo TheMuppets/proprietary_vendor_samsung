a55973a3aa1a2c403be831a298dc1f9e8eb47bd8
R. Andrew Ohana <andrew.ohana@gmail.com>
2012-12-25 02:45:09 -0800

    p68x0: update for camera wrapper and gps

--- smdk4210-tab/props/Android.mk
+++ smdk4210-tab/props/Android.mk
@@ -40,9 +40,9 @@ LOCAL_MODULE_PATH := $(TARGET_OUT)/lib
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libfimc
+LOCAL_MODULE := libUMP
 LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := system/lib/libfimc.so
+LOCAL_SRC_FILES := system/lib/libUMP.so
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
@@ -60,9 +60,9 @@ LOCAL_MODULE_PATH := $(TARGET_OUT)/lib
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libhdmi
+LOCAL_MODULE := libfimc
 LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := system/lib/libhdmi.so
+LOCAL_SRC_FILES := system/lib/libfimc.so
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
@@ -70,9 +70,9 @@ LOCAL_MODULE_PATH := $(TARGET_OUT)/lib
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libUMP
+LOCAL_MODULE := libhdmi
 LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := system/lib/libUMP.so
+LOCAL_SRC_FILES := system/lib/libhdmi.so
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
