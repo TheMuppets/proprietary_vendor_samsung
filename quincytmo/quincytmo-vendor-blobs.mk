9d3412b19355739c4f3a7b2a258f4080785a3992
Steve Kondik <shade@chemlab.org>
2013-02-06 22:54:27 -0800

    quincy: Update blobs
    Change-Id: I4fdda7c5a1a6d76a035665253d57e8902837c71c

--- quincytmo/quincytmo-vendor-blobs.mk
+++ quincytmo/quincytmo-vendor-blobs.mk
@@ -46,10 +46,12 @@ PRODUCT_COPY_FILES += \
 	vendor/samsung/quincytmo/proprietary/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
 	vendor/samsung/quincytmo/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
 	vendor/samsung/quincytmo/proprietary/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
+	vendor/samsung/quincytmo/proprietary/lib/egl/libplayback_adreno200.so:system/lib/egl/libplayback_adreno200.so \
 	vendor/samsung/quincytmo/proprietary/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
 	vendor/samsung/quincytmo/proprietary/lib/libgsl.so:system/lib/libgsl.so \
 	vendor/samsung/quincytmo/proprietary/lib/libC2D2.so:system/lib/libC2D2.so \
 	vendor/samsung/quincytmo/proprietary/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
+	vendor/samsung/quincytmo/proprietary/lib/libOpenCL.so:system/lib/libOpenCL.so \
 	vendor/samsung/quincytmo/proprietary/lib/libOpenVG.so:system/lib/libOpenVG.so \
 	vendor/samsung/quincytmo/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
 	vendor/samsung/quincytmo/proprietary/bin/netmgrd:system/bin/netmgrd \
