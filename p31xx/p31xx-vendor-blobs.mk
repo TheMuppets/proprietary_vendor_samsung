1f502e2c36c812cd577f84d34e72b4fba703ed5b
codeworkx <codeworkx@cyanogenmod.org>
2013-02-16 03:37:49 +0000

    p31xx, p51xx: add smc blobs

--- p31xx/p31xx-vendor-blobs.mk
+++ p31xx/p31xx-vendor-blobs.mk
@@ -18,7 +18,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/common/system/bin/dock_kbd_attach:system/bin/dock_kbd_attach \
     $(LOCAL_PATH)/common/system/bin/geomagneticd:system/bin/geomagneticd \
     $(LOCAL_PATH)/common/system/bin/gpsd:system/bin/gpsd \
-    $(LOCAL_PATH)/common/system/bin/orientationd:system/bin/orientationd
+    $(LOCAL_PATH)/common/system/bin/orientationd:system/bin/orientationd \
+    $(LOCAL_PATH)/common/system/bin/smc.ini:system/bin/smc.ini \
+    $(LOCAL_PATH)/common/system/bin/smc_pa.ift:system/bin/smc_pa.ift
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/common/system/etc/wifi/bcmdhd_apsta.bin:system/etc/wifi/bcmdhd_apsta.bin \
