d24d8ed638d28011366dfd7a5f7eb29219d9e72c
Tianrui Guo <tianruig@gmail.com>
2013-05-15 21:05:12 -0700

    venturi_usa -> venturi

--- venturi/venturi-vendor.mk
+++ venturi/venturi-vendor.mk
@@ -25,6 +25,6 @@ PRODUCT_COPY_FILES := \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
 
 # Pick up overlay for features that depend on non-open-source files
-DEVICE_PACKAGE_OVERLAYS := vendor/samsung/venturi_usa/overlay
+DEVICE_PACKAGE_OVERLAYS := vendor/samsung/venturi/overlay
 
-$(call inherit-product, vendor/samsung/venturi_usa/venturi_usa-vendor-blobs.mk)
+$(call inherit-product, vendor/samsung/venturi/venturi-vendor-blobs.mk)
