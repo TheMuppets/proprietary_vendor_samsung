72e4ccf620906eefe8ef5faedf014af982ba5d43
Shareef Ali <shareefalis@cyanogenmod.org>
2013-08-06 10:30:25 -0500

    d2: mf1 blobs

--- d2-common/d2-common-vendor-blobs.mk
+++ d2-common/d2-common-vendor-blobs.mk
@@ -22,7 +22,6 @@ PRODUCT_COPY_FILES += \
         vendor/samsung/d2-common/proprietary/bin/bcm4334_semco.hcd:system/bin/bcm4334_semco.hcd \
         vendor/samsung/d2-common/proprietary/lib/libacdbloader.so:system/lib/libacdbloader.so \
         vendor/samsung/d2-common/proprietary/lib/libaudcal.so:system/lib/libaudcal.so \
-        vendor/samsung/d2-common/proprietary/lib/libExtendedExtractor.so:system/lib/libExtendedExtractor.so \
         vendor/samsung/d2-common/proprietary/lib/libmmparser.so:system/lib/libmmparser.so \
         vendor/samsung/d2-common/proprietary/lib/libmmosal.so:system/lib/libmmosal.so \
         vendor/samsung/d2-common/proprietary/lib/libdivxdrm.so:system/lib/libdivxdrm.so \
@@ -32,7 +31,6 @@ PRODUCT_COPY_FILES += \
         vendor/samsung/d2-common/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
         vendor/samsung/d2-common/proprietary/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
         vendor/samsung/d2-common/proprietary/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
-        vendor/samsung/d2-common/proprietary/lib/egl/libplayback_adreno200.so:system/lib/egl/libplayback_adreno200.so \
         vendor/samsung/d2-common/proprietary/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
         vendor/samsung/d2-common/proprietary/lib/libgsl.so:system/lib/libgsl.so \
         vendor/samsung/d2-common/proprietary/lib/libC2D2.so:system/lib/libC2D2.so \
@@ -109,11 +107,9 @@ PRODUCT_COPY_FILES += \
         vendor/samsung/d2-common/proprietary/cameradata/SlimISP_GC.bin:system/cameradata/SlimISP_GC.bin \
         vendor/samsung/d2-common/proprietary/cameradata/SlimISP_GD.bin:system/cameradata/SlimISP_GD.bin \
         vendor/samsung/d2-common/proprietary/cameradata/SlimISP_GE.bin:system/cameradata/SlimISP_GE.bin \
-        vendor/samsung/d2-common/proprietary/cameradata/SlimISP_GH.bin:system/cameradata/SlimISP_GH.bin \
         vendor/samsung/d2-common/proprietary/cameradata/SlimISP_ZC.bin:system/cameradata/SlimISP_ZC.bin \
         vendor/samsung/d2-common/proprietary/cameradata/SlimISP_ZD.bin:system/cameradata/SlimISP_ZD.bin \
         vendor/samsung/d2-common/proprietary/cameradata/SlimISP_ZE.bin:system/cameradata/SlimISP_ZE.bin \
-        vendor/samsung/d2-common/proprietary/cameradata/SlimISP_ZH.bin:system/cameradata/SlimISP_ZH.bin \
         vendor/samsung/d2-common/proprietary/cameradata/datapattern_420sp.yuv:system/cameradata/datapattern_420sp.yuv \
         vendor/samsung/d2-common/proprietary/cameradata/datapattern_front_420sp.yuv:system/cameradata/datapattern_front_420sp.yuv \
         vendor/samsung/d2-common/proprietary/lib/libdiag.so:system/lib/libdiag.so \
@@ -123,9 +119,7 @@ PRODUCT_COPY_FILES += \
         vendor/samsung/d2-common/proprietary/etc/wifi/bcmdhd_mfg.bin_b2:system/etc/wifi/bcmdhd_mfg.bin_b2 \
         vendor/samsung/d2-common/proprietary/etc/wifi/nvram_mfg.txt:system/etc/wifi/nvram_mfg.txt \
         vendor/samsung/d2-common/proprietary/etc/wifi/nvram_mfg.txt_murata:system/etc/wifi/nvram_mfg.txt_murata \
-        vendor/samsung/d2-common/proprietary/etc/wifi/nvram_mfg.txt_semcosh:system/etc/wifi/nvram_mfg.txt_semcosh \
         vendor/samsung/d2-common/proprietary/etc/wifi/nvram_net.txt:system/etc/wifi/nvram_net.txt \
         vendor/samsung/d2-common/proprietary/etc/wifi/nvram_net.txt_murata:system/etc/wifi/nvram_net.txt_murata \
-        vendor/samsung/d2-common/proprietary/etc/wifi/nvram_net.txt_semcosh:system/etc/wifi/nvram_net.txt_semcosh \
         vendor/samsung/d2-common/proprietary/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
         vendor/samsung/d2-common/proprietary/lib/libqc-opt.so:system/lib/libqc-opt.so
