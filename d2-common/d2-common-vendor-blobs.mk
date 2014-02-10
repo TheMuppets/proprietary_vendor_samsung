415916c37644993c5747c1232989624126c6ef5b
Dave Daynard <nardholio@gmail.com>
2014-02-09 21:34:44 -0500

    d2-common: updated adreno from goldenlteatt
    I/Adreno200-EGL( 7568): <qeglDrvAPI_eglInitialize:265>: EGL 1.4 QUALCOMM
    build:
    AU_LINUX_ANDROID_JB_2.5.5.04.02.02.092.059_msm8960_JB_2.5.5_CL3896081_release_AU
      (CL3896081)

--- d2-common/d2-common-vendor-blobs.mk
+++ d2-common/d2-common-vendor-blobs.mk
@@ -33,11 +33,13 @@ PRODUCT_COPY_FILES += \
         vendor/samsung/d2-common/proprietary/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
         vendor/samsung/d2-common/proprietary/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
         vendor/samsung/d2-common/proprietary/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
+        vendor/samsung/d2-common/proprietary/lib/libCB.so:system/lib/libCB.so \
         vendor/samsung/d2-common/proprietary/lib/libgsl.so:system/lib/libgsl.so \
         vendor/samsung/d2-common/proprietary/lib/libC2D2.so:system/lib/libC2D2.so \
         vendor/samsung/d2-common/proprietary/lib/libOpenVG.so:system/lib/libOpenVG.so \
         vendor/samsung/d2-common/proprietary/lib/libOpenCL.so:system/lib/libOpenCL.so \
         vendor/samsung/d2-common/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
+        vendor/samsung/d2-common/proprietary/lib/libwvm.so:system/lib/libwvm.so \
         vendor/samsung/d2-common/proprietary/etc/firmware/a225p5_pm4.fw:system/etc/firmware/a225p5_pm4.fw \
         vendor/samsung/d2-common/proprietary/etc/firmware/a225_pfp.fw:system/etc/firmware/a225_pfp.fw \
         vendor/samsung/d2-common/proprietary/etc/firmware/a225_pm4.fw:system/etc/firmware/a225_pm4.fw \
