39538dd09f9e49ffbc5d508d29fcd733ab9adf99
Dan Pasanen <dan.pasanen@gmail.com>
2014-02-26 15:32:39 -0600

    d2lte: split up gsm/cdma rilblobs
    * use d2tmo for gsm
    * use r530 for cdma
    
    Change-Id: I3570051e6bf9e555bf24a524f2b47b69ba781ff9

--- d2lte/d2lte-vendor-blobs.mk
+++ d2lte/d2lte-vendor-blobs.mk
@@ -135,6 +135,8 @@ PRODUCT_COPY_FILES += \
         vendor/samsung/d2lte/proprietary/etc/wifi/nvram_net.txt_semcosh:system/etc/wifi/nvram_net.txt_semcosh \
         vendor/samsung/d2lte/proprietary/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
         vendor/samsung/d2lte/proprietary/lib/libqc-opt.so:system/lib/libqc-opt.so \
-        vendor/samsung/d2lte/proprietary/lib/libril-qc-qmi-1.so:system/lib/libril-qc-qmi-1.so \
-        vendor/samsung/d2lte/proprietary/lib/libril.so:system/lib/libril.so
+        vendor/samsung/d2lte/proprietary/blobs/cdma/lib/libril-qc-qmi-1.so:system/blobs/cdma/lib/libril-qc-qmi-1.so \
+        vendor/samsung/d2lte/proprietary/blobs/gsm/lib/libril-qc-qmi-1.so:system/blobs/gsm/lib/libril-qc-qmi-1.so \
+        vendor/samsung/d2lte/proprietary/blobs/cdma/lib/libril.so:system/blobs/cdma/lib/libril.so \
+        vendor/samsung/d2lte/proprietary/blobs/gsm/lib/libril.so:system/blobs/gsm/lib/libril.so
 
