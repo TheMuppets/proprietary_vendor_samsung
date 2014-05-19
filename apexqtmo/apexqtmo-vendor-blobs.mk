a938d7993289336ba21d7947dd9aa1fef928786c
Dave Daynard <nardholio@gmail.com>
2014-05-18 21:04:41 -0400

    d2lte: update blobs
    Do sensors, nfc, audio
    
    Leave off camera, graphics, adreno, ril

--- apexqtmo/apexqtmo-vendor-blobs.mk
+++ apexqtmo/apexqtmo-vendor-blobs.mk
@@ -38,7 +38,6 @@ PRODUCT_COPY_FILES += \
 	vendor/samsung/apexqtmo/proprietary/lib/libimage-omx-common.so:system/lib/libimage-omx-common.so \
 	vendor/samsung/apexqtmo/proprietary/lib/libaudioalsa.so:system/lib/libaudioalsa.so \
 	vendor/samsung/apexqtmo/proprietary/lib/libaudioparsers.so:system/lib/libaudioparsers.so \
-	vendor/samsung/apexqtmo/proprietary/lib/libcsd-client.so:system/lib/libcsd-client.so \
 	vendor/samsung/apexqtmo/proprietary/lib/hw/camera.vendor.msm8960.so:system/lib/hw/camera.vendor.msm8960.so \
 	vendor/samsung/apexqtmo/proprietary/lib/hw/sensors.msm8960.so:system/lib/hw/sensors.msm8960.so \
 	vendor/samsung/apexqtmo/proprietary/lib/libmmjpeg.so:system/lib/libmmjpeg.so \
