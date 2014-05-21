776b51e6d599c91f04f8f69c05638dc4c0010842
Utkarsh Gupta <utkarsh.eminem@gmail.com>
2014-05-21 21:11:29 +0530

    KitKat DRM Blobs

--- smdk4412-common/common-vendor-blobs.mk
+++ smdk4412-common/common-vendor-blobs.mk
@@ -125,6 +125,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/system/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
+    $(LOCAL_PATH)/proprietary/system/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
     $(LOCAL_PATH)/proprietary/system/vendor/lib/liblvverx.so:system/vendor/lib/liblvverx.so \
     $(LOCAL_PATH)/proprietary/system/vendor/lib/liblvvetx.so:system/vendor/lib/liblvvetx.so \
     $(LOCAL_PATH)/proprietary/system/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
