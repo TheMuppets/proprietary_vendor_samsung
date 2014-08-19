a3d3a542c7fc2d15f8de76b385a8c210a73f22f0
Davis Malnieks <malnieks.davis@gmail.com>
2014-08-19 22:12:02 +0300

    wilcox: update some blobs

--- wilcoxltexx/wilcoxltexx-vendor-blobs.mk
+++ wilcoxltexx/wilcoxltexx-vendor-blobs.mk
@@ -18,6 +18,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/wilcoxltexx/proprietary/vendor/firmware/libpn547_fw.so:system/vendor/firmware/libpn547_fw.so \
     vendor/samsung/wilcoxltexx/proprietary/vendor/firmware/libpn547_fw_pku.so:system/vendor/firmware/libpn547_fw_pku.so \
     vendor/samsung/wilcoxltexx/proprietary/vendor/firmware/libpn547_fw_platform.so:system/vendor/firmware/libpn547_fw_platform.so \
+    vendor/samsung/wilcoxltexx/proprietary/lib/hw/nfc.msm8960.so:system/lib/hw/nfc.msm8960.so \
     vendor/samsung/wilcoxltexx/proprietary/bin/btnvtool:system/bin/btnvtool \
     vendor/samsung/wilcoxltexx/proprietary/bin/fm_qsoc_patches:system/bin/fm_qsoc_patches \
     vendor/samsung/wilcoxltexx/proprietary/bin/hci_qcomm_init:system/bin/hci_qcomm_init \
