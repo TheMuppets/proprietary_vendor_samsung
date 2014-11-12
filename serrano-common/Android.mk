d1204b5a4f21def691c5239246b061c1577d3b6b
Arne Coucheron <arco68@gmail.com>
2014-11-12 09:54:15 +0100

    serrano: Add serranolteusc to build barrier

--- serrano-common/Android.mk
+++ serrano-common/Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter serrano3gxx serranodsdd serranodsub serranoltebmc serranoltespr serranoltexx,$(TARGET_DEVICE)),)
+ifneq ($(filter serrano3gxx serranodsdd serranodsub serranoltebmc serranoltespr serranolteusc serranoltexx,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libtime_genoff
