9fb884683720da342bba79525b619a598528de62
Dan Pasanen <dan.pasanen@gmail.com>
2014-12-05 10:51:03 -0600

    jf-common: add jfltexx to build barrier
    Change-Id: I397abd8206a0d9946b11f7d37342b98500138fe1

--- jf-common/Android.mk
+++ jf-common/Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter jactivelte jflte jflteatt jfltecan jfltecri jfltecsp jfltegsm jfltespr jfltetmo jflteusc jfltevzw,$(TARGET_DEVICE)),)
+ifneq ($(filter jactivelte jflte jflteatt jfltecan jfltecri jfltecsp jfltegsm jfltespr jfltetmo jflteusc jfltevzw jfltexx,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libtime_genoff
