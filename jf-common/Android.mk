3a67d0a4f11e59271714755f7c99e73f448776e2
dcd <dcd1182@gmail.com>
2014-12-17 10:13:21 -0600

    jf-common: update blobs to I9505GUEUDNL3

--- jf-common/Android.mk
+++ jf-common/Android.mk
@@ -21,7 +21,7 @@ ifneq ($(filter jactivelte jflte jflteatt jfltecan jfltecri jfltecsp jfltegsm jf
 include $(CLEAR_VARS)
 LOCAL_MODULE := libtime_genoff
 LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := proprietary/lib/libtime_genoff.so
+LOCAL_SRC_FILES := proprietary/vendor/lib/libtime_genoff.so
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
