a2524840447301d78b7562c2f248f5af1ebda64b
Andreas Bläsius <skate4life@gmx.de>
2014-12-29 17:06:49 +0100

    P31XX & P51XX: use Open source Samsung dock keyboard attach program

--- p31xx/p31xx-vendor-blobs.mk
+++ p31xx/p31xx-vendor-blobs.mk
@@ -15,7 +15,6 @@
 LOCAL_PATH := vendor/samsung/p31xx
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/common/system/bin/dock_kbd_attach:system/bin/dock_kbd_attach \
     $(LOCAL_PATH)/common/system/bin/geomagneticd:system/bin/geomagneticd \
     $(LOCAL_PATH)/common/system/bin/gpsd:system/bin/gpsd \
     $(LOCAL_PATH)/common/system/bin/orientationd:system/bin/orientationd \
