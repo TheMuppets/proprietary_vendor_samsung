a68babe49dd06ffe362b841202b652e5f67349f6
Davis Malnieks <malnieks.davis@gmail.com>
2015-01-06 17:21:08 +0200

    wilcoxltexx: sync adreno with serrano

--- wilcoxltexx/wilcoxltexx-vendor-blobs.mk
+++ wilcoxltexx/wilcoxltexx-vendor-blobs.mk
@@ -40,10 +40,11 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/wilcoxltexx/proprietary/vendor/lib/egl/libq3dtools_adreno.so:system/vendor/lib/egl/libq3dtools_adreno.so \
     vendor/samsung/wilcoxltexx/proprietary/vendor/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
     vendor/samsung/wilcoxltexx/proprietary/vendor/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
-    vendor/samsung/wilcoxltexx/proprietary/vendor/lib/libc2d2_a3xx.so:system/vendor/lib/libc2d2_a3xx.so \
+    vendor/samsung/wilcoxltexx/proprietary/vendor/lib/libCB.so:system/vendor/lib/libCB.so \
+    vendor/samsung/wilcoxltexx/proprietary/vendor/lib/libc2d30-a3xx.so:system/vendor/lib/libc2d30-a3xx.so \
+    vendor/samsung/wilcoxltexx/proprietary/vendor/lib/libc2d30.so:system/vendor/lib/libc2d30.so \
     vendor/samsung/wilcoxltexx/proprietary/vendor/lib/libgsl.so:system/vendor/lib/libgsl.so \
     vendor/samsung/wilcoxltexx/proprietary/vendor/lib/libllvm-a3xx.so:system/vendor/lib/libllvm-a3xx.so \
-    vendor/samsung/wilcoxltexx/proprietary/vendor/lib/libCB.so:system/vendor/lib/libCB.so \
     vendor/samsung/wilcoxltexx/proprietary/vendor/lib/libOpenCL.so:system/vendor/lib/libOpenCL.so \
     vendor/samsung/wilcoxltexx/proprietary/vendor/lib/libOpenVG.so:system/vendor/lib/libOpenVG.so \
     vendor/samsung/wilcoxltexx/proprietary/vendor/lib/libsc-a3xx.so:system/vendor/lib/libsc-a3xx.so \
