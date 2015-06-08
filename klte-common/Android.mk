52aa2a68c69c14516bad6709412dd71d41701519
scoty755 <scoty755@gmail.com>
2015-06-08 20:01:42 +0900

    kltekdi: Import proprietary libs

--- klte-common/Android.mk
+++ klte-common/Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter klte kltechn kltechnduo kltedv kltespr klteusc kltevzw,$(TARGET_DEVICE)),)
+ifneq ($(filter klte kltechn kltechnduo kltedv kltekdi kltespr klteusc kltevzw,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := TimeService
