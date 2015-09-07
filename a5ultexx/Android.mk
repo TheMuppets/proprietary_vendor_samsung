2720738e060ce702c0cfeded432991f8ac011efc
Davis Malnieks <malnieks.davis@gmail.com>
2015-09-07 20:02:44 +0300

    a5ultexx: clean-up

--- a5ultexx/Android.mk
+++ a5ultexx/Android.mk
@@ -20,17 +20,6 @@ ifeq ($(BOARD_VENDOR),samsung)
 ifneq ($(filter a5ultexx,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := com.qualcomm.location
-LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := proprietary/priv-app/com.qualcomm.location/com.qualcomm.location.apk
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_CLASS := APPS
-LOCAL_PRIVILEGED_MODULE := true
-LOCAL_CERTIFICATE := platform
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := libtime_genoff
 LOCAL_MODULE_OWNER := samsung
 LOCAL_SRC_FILES := proprietary/vendor/lib/libtime_genoff.so
--- a5ultexx/a5ultexx-vendor.mk
+++ a5ultexx/a5ultexx-vendor.mk
@@ -18,9 +18,6 @@
 DEVICE_PACKAGE_OVERLAYS += vendor/samsung/a5ultexx/overlay
 
 PRODUCT_PACKAGES += \
-    com.qualcomm.location
-
-PRODUCT_PACKAGES += \
     libtime_genoff \
     TimeService
 
