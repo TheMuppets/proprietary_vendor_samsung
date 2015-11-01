54629296f142cea9ed90897b8f8993b85bc6ff1e
Dan Pasanen <dan.pasanen@gmail.com>
2015-10-31 19:01:24 -0500

    jf: update blobs and de-commonize
    * common: de-commonize RIL and QMI
    * jf-common: remove thermal-engine
    * jf-common: update from I9505XXUHOH2
    * jf-gsm: update from I9505XXUHOH2
    * jfltespr: update from L720VPUGOH1
    * jflteusc: update from R970TYUGOE2
    * jfltezvw: update from I545VRUGOF1
    * jfltevzw: add vidc firmware
    
    Change-Id: Id25c7830cc7620ea4b64744f360f2d231b021743

--- jfltevzw/BoardConfigVendor.mk
+++ jfltevzw/BoardConfigVendor.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2015 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
