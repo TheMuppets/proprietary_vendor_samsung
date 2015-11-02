6bc8a628fb77a906ca638b2d20d9b8f0e085cd74
Dan Pasanen <dan.pasanen@gmail.com>
2015-11-01 20:33:48 -0500

    mondrianwifi: update adreno from hammerhead-mra58k
    Change-Id: I4ff2171d9f7d5571c32689f83e2b51ad503243e0

--- mondrianwifi/mondrianwifi-vendor-blobs.mk
+++ mondrianwifi/mondrianwifi-vendor-blobs.mk
@@ -66,6 +66,7 @@ PRODUCT_COPY_FILES += \
 	vendor/samsung/mondrianwifi/proprietary/vendor/lib/libbccQTI.so:system/vendor/lib/libbccQTI.so \
 	vendor/samsung/mondrianwifi/proprietary/vendor/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
 	vendor/samsung/mondrianwifi/proprietary/vendor/lib/libc2d30-a3xx.so:system/vendor/lib/libc2d30-a3xx.so \
+	vendor/samsung/mondrianwifi/proprietary/vendor/lib/libc2d30.so:system/vendor/lib/libc2d30.so \
 	vendor/samsung/mondrianwifi/proprietary/vendor/lib/libCB.so:system/vendor/lib/libCB.so \
 	vendor/samsung/mondrianwifi/proprietary/vendor/lib/libgsl.so:system/vendor/lib/libgsl.so \
 	vendor/samsung/mondrianwifi/proprietary/vendor/lib/libllvm-qcom.so:system/vendor/lib/libllvm-qcom.so \
