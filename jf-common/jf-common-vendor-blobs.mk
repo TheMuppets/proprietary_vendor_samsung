61ebe8e8e17d394583b1007c6c4a9c4c892bd5dd
NucWin <nucwin@gmail.com>
2015-11-20 16:11:34 +0000

    jf: blob gps from OK1

--- jf-common/jf-common-vendor-blobs.mk
+++ jf-common/jf-common-vendor-blobs.mk
@@ -114,7 +114,11 @@ PRODUCT_COPY_FILES += \
 	vendor/samsung/jf-common/proprietary/vendor/firmware/a300_pm4.fw:system/vendor/firmware/a300_pm4.fw \
 	vendor/samsung/jf-common/proprietary/vendor/firmware/vidc_1080p.fw:system/vendor/firmware/vidc_1080p.fw \
 	vendor/samsung/jf-common/proprietary/bin/loc_launcher:system/bin/loc_launcher \
+	vendor/samsung/jf-common/proprietary/lib/hw/gps.default.so:system/lib/hw/gps.default.so \
+	vendor/samsung/jf-common/proprietary/lib/libgps.utils.so:system/lib/libgps.utils.so \
 	vendor/samsung/jf-common/proprietary/lib/libloc_api_v02.so:system/lib/libloc_api_v02.so \
+	vendor/samsung/jf-common/proprietary/lib/libloc_eng.so:system/lib/libloc_eng.so \
+	vendor/samsung/jf-common/proprietary/lib/libloc_core.so:system/lib/libloc_core.so \
 	vendor/samsung/jf-common/proprietary/vendor/lib/libgeofence.so:system/vendor/lib/libgeofence.so \
 	vendor/samsung/jf-common/proprietary/vendor/lib/libizat_core.so:system/vendor/lib/libizat_core.so \
 	vendor/samsung/jf-common/proprietary/vendor/lib/liblbs_core.so:system/vendor/lib/liblbs_core.so \
