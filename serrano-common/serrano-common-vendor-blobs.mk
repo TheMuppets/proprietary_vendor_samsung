0c3bc36a6ceadf009c8115f2bfa7a9aa055ebd38
Arne Coucheron <arco68@gmail.com>
2015-11-27 05:43:31 +0100

    serrano: Kill mm-parser blobs

--- serrano-common/serrano-common-vendor-blobs.mk
+++ serrano-common/serrano-common-vendor-blobs.mk
@@ -23,11 +23,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/serrano-common/proprietary/bin/thermald:system/bin/thermald \
     vendor/samsung/serrano-common/proprietary/lib/libacdbloader.so:system/lib/libacdbloader.so \
     vendor/samsung/serrano-common/proprietary/lib/libaudcal.so:system/lib/libaudcal.so \
-    vendor/samsung/serrano-common/proprietary/lib/libDivxDrm.so:system/lib/libDivxDrm.so \
-    vendor/samsung/serrano-common/proprietary/lib/libExtendedExtractor.so:system/lib/libExtendedExtractor.so \
-    vendor/samsung/serrano-common/proprietary/lib/libSHIMDivxDrm.so:system/lib/libSHIMDivxDrm.so \
-    vendor/samsung/serrano-common/proprietary/lib/libmmparser.so:system/lib/libmmparser.so \
-    vendor/samsung/serrano-common/proprietary/lib/libmmosal.so:system/lib/libmmosal.so \
     vendor/samsung/serrano-common/proprietary/etc/firmware/a300_pfp.fw:system/etc/firmware/a300_pfp.fw \
     vendor/samsung/serrano-common/proprietary/etc/firmware/a300_pm4.fw:system/etc/firmware/a300_pm4.fw \
     vendor/samsung/serrano-common/proprietary/etc/firmware/vidc_1080p.fw:system/etc/firmware/vidc_1080p.fw \
