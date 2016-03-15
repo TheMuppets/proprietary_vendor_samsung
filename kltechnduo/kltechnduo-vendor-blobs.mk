40bd981fa4a6ff82a031652e76acba9c1bcfae25
ljzyal <ljzyal@gmail.com>
2016-03-15 22:19:28 +0800

    kltechnduo: Add g9009w's own ril lib

--- kltechnduo/kltechnduo-vendor-blobs.mk
+++ kltechnduo/kltechnduo-vendor-blobs.mk
@@ -76,7 +76,9 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/kltechnduo/proprietary/lib/libril.so:system/lib/libril.so \
     vendor/samsung/kltechnduo/proprietary/lib/librilutils.so:system/lib/librilutils.so \
     vendor/samsung/kltechnduo/proprietary/lib/libsec-ril.so:system/lib/libsec-ril.so \
+    vendor/samsung/kltechnduo/proprietary/lib/libsec-ril-09w.so:system/lib/libsec-ril-09w.so \
     vendor/samsung/kltechnduo/proprietary/lib/libsec-ril-dsds.so:system/lib/libsec-ril-dsds.so \
+    vendor/samsung/kltechnduo/proprietary/lib/libsec-ril-dsds-09w.so:system/lib/libsec-ril-dsds-09w.so \
     vendor/samsung/kltechnduo/proprietary/lib/libsecnativefeature.so:system/lib/libsecnativefeature.so \
     vendor/samsung/kltechnduo/proprietary/lib/libsecril-client.so:system/lib/libsecril-client.so \
     vendor/samsung/kltechnduo/proprietary/vendor/lib/libconfigdb.so:system/vendor/lib/libconfigdb.so \
