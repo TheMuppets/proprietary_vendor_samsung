1a363569dd7946f782a8d3af52a1abab1777f48d
Kristoffer Grundström <kristoffer.grundstrom1983@gmail.com>
2016-03-18 00:43:22 +0100

    Created & added the Android makefile for the board
    Signed-off-by: Kristoffer Grundström <kristoffer.grundstrom1983@gmail.com>

---
+++ zenlte/AndroidBoard.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
+
+# include the non-open-source counterpart to this file
+-include vendor/samsung/zenlte/AndroidBoardVendor.mk
