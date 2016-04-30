921af964b1c3010b336a7d9ceff83c62720ebe06
ZION959 <ziontran@gmail.com>
2016-04-29 23:05:39 -0700

    trlte varients: Fix bluetooth share fc after CM merge (6.0.1r30)
    Signed-off-by: ZION959 <ziontran@gmail.com>

--- trltetmo/trltetmo-vendor-blobs.mk
+++ trltetmo/trltetmo-vendor-blobs.mk
@@ -19,8 +19,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/trltetmo/proprietary/vendor/firmware/bcm43569.hcd:system/vendor/firmware/bcm43569.hcd \
     vendor/samsung/trltetmo/proprietary/vendor/firmware/bcm43569A0.hcd:system/vendor/firmware/bcm43569A0.hcd \
     vendor/samsung/trltetmo/proprietary/vendor/firmware/bcm43569A1.hcd:system/vendor/firmware/bcm43569A1.hcd \
-    vendor/samsung/trltetmo/proprietary/lib/hw/bluetooth.default.so:system/lib/hw/bluetooth.default.so \
-    vendor/samsung/trltetmo/proprietary/lib/libbluetooth_jni.so:system/lib/libbluetooth_jni.so \
     vendor/samsung/trltetmo/proprietary/bin/gsiff_daemon:system/bin/gsiff_daemon \
     vendor/samsung/trltetmo/proprietary/lib/libloc_api_v02.so:system/lib/libloc_api_v02.so \
     vendor/samsung/trltetmo/proprietary/vendor/lib/hw/flp.default.so:system/vendor/lib/hw/flp.default.so \
--- trlteusc/trlteusc-vendor-blobs.mk
+++ trlteusc/trlteusc-vendor-blobs.mk
@@ -19,8 +19,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/trlteusc/proprietary/vendor/firmware/bcm43569.hcd:system/vendor/firmware/bcm43569.hcd \
     vendor/samsung/trlteusc/proprietary/vendor/firmware/bcm43569A0.hcd:system/vendor/firmware/bcm43569A0.hcd \
     vendor/samsung/trlteusc/proprietary/vendor/firmware/bcm43569A1.hcd:system/vendor/firmware/bcm43569A1.hcd \
-    vendor/samsung/trlteusc/proprietary/lib/hw/bluetooth.default.so:system/lib/hw/bluetooth.default.so \
-    vendor/samsung/trlteusc/proprietary/lib/libbluetooth_jni.so:system/lib/libbluetooth_jni.so \
     vendor/samsung/trlteusc/proprietary/vendor/lib/hw/flp.default.so:system/vendor/lib/hw/flp.default.so \
     vendor/samsung/trlteusc/proprietary/lib/libloc_api_v02.so:system/lib/libloc_api_v02.so \
     vendor/samsung/trlteusc/proprietary/vendor/lib/libgeofence.so:system/vendor/lib/libgeofence.so \
