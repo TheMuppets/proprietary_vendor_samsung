379d9a3fb6aaa74c1c454452766dfd5038970b9d
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-05-31 07:21:48 -0500

    jf: update adreno from adreno_nexus_may2016.zip
    * Adreno-Nexus7-MOB30J.zip
    
    Change-Id: Ica3b570a1534f0a9aeaf9da3e96904276aa44d96

--- jf-common/jf-common-vendor-blobs.mk
+++ jf-common/jf-common-vendor-blobs.mk
@@ -95,6 +95,7 @@ PRODUCT_COPY_FILES += \
 	vendor/samsung/jf-common/proprietary/vendor/lib/egl/libGLESv2_adreno.so:system/vendor/lib/egl/libGLESv2_adreno.so \
 	vendor/samsung/jf-common/proprietary/vendor/lib/egl/libq3dtools_adreno.so:system/vendor/lib/egl/libq3dtools_adreno.so \
 	vendor/samsung/jf-common/proprietary/vendor/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
+	vendor/samsung/jf-common/proprietary/vendor/lib/libbccQTI.so:system/vendor/lib/libbccQTI.so \
 	vendor/samsung/jf-common/proprietary/vendor/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
 	vendor/samsung/jf-common/proprietary/vendor/lib/libc2d30-a3xx.so:system/vendor/lib/libc2d30-a3xx.so \
 	vendor/samsung/jf-common/proprietary/vendor/lib/libc2d30.so:system/vendor/lib/libc2d30.so \
