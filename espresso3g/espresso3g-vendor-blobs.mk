64ae3aa4ade5ee6dc05367cbb5d7948d9ae84145
Ziyan <jaraidaniel@gmail.com>
2016-09-09 17:19:36 +0200

    espresso3g: move blobs to /vendor
    Change-Id: I5194af96941967319e66f8912d41c9f2edebd0fb

--- espresso3g/espresso3g-vendor-blobs.mk
+++ espresso3g/espresso3g-vendor-blobs.mk
@@ -15,5 +15,5 @@
 # RIL
 PRODUCT_COPY_FILES += \
     vendor/samsung/espresso3g/proprietary/sbin/cbd:root/sbin/cbd \
-    vendor/samsung/espresso3g/proprietary/system/lib/libsec-ril.so:system/lib/libsec-ril.so \
-    vendor/samsung/espresso3g/proprietary/system/lib/libsecnativefeature.so:system/lib/libsecnativefeature.so
+    vendor/samsung/espresso3g/proprietary/system/vendor/lib/libsec-ril.so:system/vendor/lib/libsec-ril.so \
+    vendor/samsung/espresso3g/proprietary/system/lib/vendor/libsecnativefeature.so:system/vendor/lib/libsecnativefeature.so
