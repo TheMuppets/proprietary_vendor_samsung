cf69052b6489bab8b56f9749824670065e033917
Andreas Blaesius <skate4life@gmx.de>
2016-10-09 10:55:12 +0200

    Revert "espresso-common: add libstlport"
    - our gps blob still needs it, but we are going to compile it from source

--- espresso-common/common-vendor-blobs.mk
+++ espresso-common/common-vendor-blobs.mk
@@ -32,7 +32,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     vendor/samsung/espresso-common/proprietary/system/lib/libdrmdecrypt.so:system/lib/libdrmdecrypt.so \
-    vendor/samsung/espresso-common/proprietary/system/vendor/lib/libstlport.so:system/vendor/lib/libstlport.so \
     vendor/samsung/espresso-common/proprietary/system/vendor/lib/libdrmmtkutil.so:system/vendor/lib/libdrmmtkutil.so \
     vendor/samsung/espresso-common/proprietary/system/vendor/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
     vendor/samsung/espresso-common/proprietary/system/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
