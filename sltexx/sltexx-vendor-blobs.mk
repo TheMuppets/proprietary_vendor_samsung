67610ea7d01409f7949072d7a60ca568e3c3a09b
Andreas Schneider <asn@cryptomilk.org>
2017-02-10 11:43:04 +0100

    sltexx: Import blobs from LL_CGP1
    Change-Id: I43d324097e43e2fa72449e8add7b41bebffaac27

--- sltexx/sltexx-vendor-blobs.mk
+++ sltexx/sltexx-vendor-blobs.mk
@@ -1,5 +1,4 @@
-#
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2012 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
