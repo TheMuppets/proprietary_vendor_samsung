3351a2d181e9343516c660af57fc9db14d689732
deadman96385 <seanhoyt963@gmail.com>
2017-02-14 19:51:45 -0600

    Update klimtwifi to the latest
    Change-Id: I8ff398a7a2dcddb6c62d7d2c9ac50a6888689339

--- klimtwifi/klimtwifi-vendor.mk
+++ klimtwifi/klimtwifi-vendor.mk
@@ -1,4 +1,5 @@
 # Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,3 +19,4 @@
 DEVICE_PACKAGE_OVERLAYS := vendor/samsung/klimtwifi/overlay
 
 $(call inherit-product, vendor/samsung/klimtwifi/klimtwifi-vendor-blobs.mk)
+$(call inherit-product, vendor/samsung/exynos5420-common/exynos5420-common-vendor.mk)
