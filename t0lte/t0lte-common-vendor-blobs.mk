259ad0e3c8aa16f65dadc5606b15b87d8f89ef69
Jason Lu <jasonbangbang@gmail.com>
2017-03-06 16:37:53 -0600

    t0lte: Clean up unused files and move keylayout to device tree
    sec_e-pen.idc and sec_e-pen.kl aren't even being referenced by any Makefile,
    and are not used.
    
    Change-Id: Idf379f095b0c41d3d008fdb7f27ffb25438e02b7

--- t0lte/t0lte-common-vendor-blobs.mk
+++ t0lte/t0lte-common-vendor-blobs.mk
@@ -47,6 +47,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/system/lib/libril-qcril-hook-oem.so:system/lib/libril-qcril-hook-oem.so \
     $(LOCAL_PATH)/proprietary/system/lib/libsec-ril.so:system/lib/libsec-ril.so \
     $(LOCAL_PATH)/proprietary/system/lib/libxml.so:system/lib/libxml.so
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/proprietary/system/usr/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl
