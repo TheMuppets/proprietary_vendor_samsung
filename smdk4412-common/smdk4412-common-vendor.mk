e41e71f72f6c61a1a5e3de495c3993190da0ad2c
Jason Lu <jasonbangbang@gmail.com>
2017-04-15 09:28:48 -0500

    smdk4412-common: Update Widevine
    Uses libwvdrmengine.so from d2-common.
    
    Change-Id: I55bd7eb1e8f8414697ccf993fb9877ffd3c99932
    Signed-off-by: Jason Lu <jasonbangbang@gmail.com>

--- smdk4412-common/smdk4412-common-vendor.mk
+++ smdk4412-common/smdk4412-common-vendor.mk
@@ -64,8 +64,4 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/smdk4412-common/proprietary/vendor/firmware/fimc_is_fw.bin:system/vendor/firmware/fimc_is_fw.bin \
     vendor/samsung/smdk4412-common/proprietary/vendor/firmware/mfc_fw.bin:system/vendor/firmware/mfc_fw.bin \
     vendor/samsung/smdk4412-common/proprietary/vendor/firmware/setfile.bin:system/vendor/firmware/setfile.bin \
-    vendor/samsung/smdk4412-common/proprietary/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
-    vendor/samsung/smdk4412-common/proprietary/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
-    vendor/samsung/smdk4412-common/proprietary/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
-    vendor/samsung/smdk4412-common/proprietary/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
     vendor/samsung/smdk4412-common/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so
