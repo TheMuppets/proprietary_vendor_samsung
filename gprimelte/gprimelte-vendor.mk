ef8079137e44f12d1fd3fe8f6d09a26fa9699d0d
Vincent Zvikaramba <zvikovincent@gmail.com>
2017-07-12 20:54:31 +0200

    gprimelte: Rename camera.vendor.msm8916.so to camera.msm8916.so
    Change-Id: I9363dc5b86e734e5943353d364ebcbad41ca9061

--- gprimelte/gprimelte-vendor.mk
+++ gprimelte/gprimelte-vendor.mk
@@ -28,7 +28,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/gprimelte/proprietary/etc/calib_SI_matrix_data_file.dat:system/etc/calib_SI_matrix_data_file.dat \
     vendor/samsung/gprimelte/proprietary/etc/firmware/nfc_test.bin:system/etc/firmware/nfc_test.bin \
     vendor/samsung/gprimelte/proprietary/etc/sec_s3fwrn5_rfreg.bin:system/etc/sec_s3fwrn5_rfreg.bin \
-    vendor/samsung/gprimelte/proprietary/lib/hw/camera.vendor.msm8916.so:system/lib/hw/camera.vendor.msm8916.so \
+    vendor/samsung/gprimelte/proprietary/lib/hw/camera.msm8916.so:system/lib/hw/camera.msm8916.so \
     vendor/samsung/gprimelte/proprietary/lib/hw/nfc_nci.msm8916.so:system/lib/hw/nfc_nci.msm8916.so \
     vendor/samsung/gprimelte/proprietary/lib/hw/sensors.msm8916.so:system/lib/hw/sensors.msm8916.so \
     vendor/samsung/gprimelte/proprietary/lib/libAl_Awb.so:system/lib/libAl_Awb.so \
