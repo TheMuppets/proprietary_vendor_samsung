cbb02d7ad91c8c973fc3565cc184fe49cdfbafd4
Vincent Zvikaramba <zvikovincent@gmail.com>
2017-07-12 20:54:31 +0200

    gtesqltespr: Rename camera.vendor.msm8916.so to camera.msm8916.so
    Change-Id: I24f570e0046002b30d0d6ccc90557aa09b1feee8

--- gtesqltespr/gtesqltespr-vendor.mk
+++ gtesqltespr/gtesqltespr-vendor.mk
@@ -23,7 +23,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/gtesqltespr/proprietary/etc/Hdmi_cal.acdb:system/etc/Hdmi_cal.acdb \
     vendor/samsung/gtesqltespr/proprietary/etc/Headset_cal.acdb:system/etc/Headset_cal.acdb \
     vendor/samsung/gtesqltespr/proprietary/etc/Speaker_cal.acdb:system/etc/Speaker_cal.acdb \
-    vendor/samsung/gtesqltespr/proprietary/lib/hw/camera.vendor.msm8916.so:system/lib/hw/camera.vendor.msm8916.so \
+    vendor/samsung/gtesqltespr/proprietary/lib/hw/camera.msm8916.so:system/lib/hw/camera.msm8916.so \
     vendor/samsung/gtesqltespr/proprietary/lib/hw/sensors.msm8916.so:system/lib/hw/sensors.msm8916.so \
     vendor/samsung/gtesqltespr/proprietary/lib/libAl_Awb.so:system/lib/libAl_Awb.so \
     vendor/samsung/gtesqltespr/proprietary/lib/libAl_Awb_Sp.so:system/lib/libAl_Awb_Sp.so \
