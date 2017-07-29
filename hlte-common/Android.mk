4c388203fa5485df5cc3db342dba80b06eb41c53
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2017-07-29 06:57:38 -0600

    hltechn: Initial blob import
    * Samsung Package Version: N9008VZMSDQD2_N9008VCHMDQD2_CHM
    
    Change-Id: I09d4a9a0ddfde98509deb6ee3cc18f27a3d8e258

--- hlte-common/Android.mk
+++ hlte-common/Android.mk
@@ -17,7 +17,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter hlte hltetmo,$(TARGET_DEVICE)),)
+ifneq ($(filter hlte hltechn hltetmo,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libmm-abl
