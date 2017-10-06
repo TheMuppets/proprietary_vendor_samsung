f96cbc42e2b9a1b3dc525ba778a320824e68f0a5
Arne Coucheron <arco68@gmail.com>
2017-10-07 01:14:48 +0200

    serrano-common: Move libtime_genoff.so to system
    In Oreo, we get build warning if it's in vendor.

--- serrano-common/Android.mk
+++ serrano-common/Android.mk
@@ -24,7 +24,7 @@ ifneq ($(filter serrano3gxx serranodsdd serranodsub serranoltebmc \
 include $(CLEAR_VARS)
 LOCAL_MODULE := libtime_genoff
 LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := proprietary/vendor/lib/libtime_genoff.so
+LOCAL_SRC_FILES := proprietary/lib/libtime_genoff.so
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
