c99d5cb6bd66e7f3a6ee88094a7fc730aa29f470
Kevin F. Haggerty <haggertk@lineageos.org>
2017-10-23 22:21:18 -0600

    hltechn: Drop QC Framework blobs to
    * These have been moved to -common
    
    Change-Id: I7c1a48b921e11339f592ceb5aeea9beda4204e39

--- hltechn/hltechn-vendor.mk
+++ hltechn/hltechn-vendor.mk
@@ -22,22 +22,9 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/hltechn/proprietary/bin/rfs_access:system/bin/rfs_access \
     vendor/samsung/hltechn/proprietary/bin/rild:system/bin/rild \
     vendor/samsung/hltechn/proprietary/bin/rmt_storage:system/bin/rmt_storage \
-    vendor/samsung/hltechn/proprietary/lib/libmdmdetect.so:system/lib/libmdmdetect.so \
     vendor/samsung/hltechn/proprietary/lib/libreference-ril.so:system/lib/libreference-ril.so \
     vendor/samsung/hltechn/proprietary/lib/libril.so:system/lib/libril.so \
     vendor/samsung/hltechn/proprietary/lib/libsec-ril.so:system/lib/libsec-ril.so \
     vendor/samsung/hltechn/proprietary/lib/libsecnativefeature.so:system/lib/libsecnativefeature.so \
     vendor/samsung/hltechn/proprietary/lib/libsecril-client.so:system/lib/libsecril-client.so \
-    vendor/samsung/hltechn/proprietary/vendor/lib/libconfigdb.so:system/vendor/lib/libconfigdb.so \
-    vendor/samsung/hltechn/proprietary/vendor/lib/libdiag.so:system/vendor/lib/libdiag.so \
-    vendor/samsung/hltechn/proprietary/vendor/lib/libdsutils.so:system/vendor/lib/libdsutils.so \
-    vendor/samsung/hltechn/proprietary/vendor/lib/libidl.so:system/vendor/lib/libidl.so \
-    vendor/samsung/hltechn/proprietary/vendor/lib/libqcci_legacy.so:system/vendor/lib/libqcci_legacy.so \
-    vendor/samsung/hltechn/proprietary/vendor/lib/libqmi.so:system/vendor/lib/libqmi.so \
-    vendor/samsung/hltechn/proprietary/vendor/lib/libqmi_cci.so:system/vendor/lib/libqmi_cci.so \
-    vendor/samsung/hltechn/proprietary/vendor/lib/libqmi_client_qmux.so:system/vendor/lib/libqmi_client_qmux.so \
-    vendor/samsung/hltechn/proprietary/vendor/lib/libqmi_common_so.so:system/vendor/lib/libqmi_common_so.so \
-    vendor/samsung/hltechn/proprietary/vendor/lib/libqmi_csi.so:system/vendor/lib/libqmi_csi.so \
-    vendor/samsung/hltechn/proprietary/vendor/lib/libqmi_encdec.so:system/vendor/lib/libqmi_encdec.so \
-    vendor/samsung/hltechn/proprietary/vendor/lib/libqmiservices.so:system/vendor/lib/libqmiservices.so \
     vendor/samsung/hltechn/proprietary/vendor/lib/libril-qcril-hook-oem.so:system/vendor/lib/libril-qcril-hook-oem.so
