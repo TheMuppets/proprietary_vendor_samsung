43ca064971733499493e5d3806e8455937c00dd4
Vasyl Gello <vasek.gello@gmail.com>
2018-08-17 10:48:33 +0300

    v1awifi: Bring vendor blobs up-to-date
    * mcRegistry blobs are device-specific now
    
    * NXP noise suppression configuration is now common
      for all devices as NXP shared libraries must use
      only the configuration which was shipped with them

--- v1awifi/v1awifi-vendor.mk
+++ v1awifi/v1awifi-vendor.mk
@@ -1,4 +1,5 @@
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2014-2016 The CyanogenMod Project
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,4 +19,3 @@
 DEVICE_PACKAGE_OVERLAYS := vendor/samsung/v1awifi/overlay
 
 $(call inherit-product, vendor/samsung/v1awifi/v1awifi-vendor-blobs.mk)
-$(call inherit-product, vendor/samsung/exynos5420-common/exynos5420-common-vendor.mk)
