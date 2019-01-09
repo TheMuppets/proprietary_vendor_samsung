3fba1894365bbb85328de499a1ff1d094aabe0ab
Arne Coucheron <arco68@gmail.com>
2019-01-09 13:39:11 +0100

    serrano: Regenerate after moving to Lineage extract script

--- serrano-common/Android.mk
+++ serrano-common/Android.mk
@@ -1,5 +1,5 @@
-# Copyright (C) 2013-2016, The CyanogenMod Project
-# Copyright (C) 2017, The LineageOS Project
+# Copyright (C) 2013-2016 The CyanogenMod Project
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,9 +17,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(BOARD_VENDOR),samsung)
 ifneq ($(filter serrano3gxx serranodsdd serranodsub serranoltebmc \
                 serranoltespr serranolteusc serranoltexx,$(TARGET_DEVICE)),)
 
-endif
 endif
