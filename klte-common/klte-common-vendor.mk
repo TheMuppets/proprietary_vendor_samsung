09bb10dd1e94be60d8039945e43c54a4f9aaf95a
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2019-02-08 17:26:38 -0700

    klte-common: Correct semcove->semco3rd BT FW filename
    * Regen all klte* makefiles while we are here
    
    Change-Id: I03f7cc8de0ee9c86b78811c9db387e32d23f9189

--- klte-common/klte-common-vendor.mk
+++ klte-common/klte-common-vendor.mk
@@ -165,11 +165,11 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/klte-common/proprietary/vendor/lib/libqomx_jpegenc.so:system/vendor/lib/libqomx_jpegenc.so \
     vendor/samsung/klte-common/proprietary/vendor/lib/libvdis.so:system/vendor/lib/libvdis.so \
     vendor/samsung/klte-common/proprietary/vendor/bin/thermal-engine:system/vendor/bin/thermal-engine \
+    vendor/samsung/klte-common/proprietary/vendor/firmware/bcm4350_V0395.0797.hcd:system/vendor/firmware/bcm4350_V0395.0797.hcd \
     vendor/samsung/klte-common/proprietary/vendor/firmware/bcm4350_V0395.0797_murata.hcd:system/vendor/firmware/bcm4350_V0395.0797_murata.hcd \
     vendor/samsung/klte-common/proprietary/vendor/firmware/bcm4350_V0395.0797_semco.hcd:system/vendor/firmware/bcm4350_V0395.0797_semco.hcd \
+    vendor/samsung/klte-common/proprietary/vendor/firmware/bcm4350_V0395.0797_semco3rd.hcd:system/vendor/firmware/bcm4350_V0395.0797_semco3rd.hcd \
     vendor/samsung/klte-common/proprietary/vendor/firmware/bcm4350_V0395.0797_semcosh.hcd:system/vendor/firmware/bcm4350_V0395.0797_semcosh.hcd \
-    vendor/samsung/klte-common/proprietary/vendor/firmware/bcm4350_V0395.0797_semcove.hcd:system/vendor/firmware/bcm4350_V0395.0797_semcove.hcd \
-    vendor/samsung/klte-common/proprietary/vendor/firmware/bcm4350_V0395.0797.hcd:system/vendor/firmware/bcm4350_V0395.0797.hcd \
     vendor/samsung/klte-common/proprietary/vendor/firmware/bcm4350_V0395.0798_wisol.hcd:system/vendor/firmware/bcm4350_V0395.0798_wisol.hcd \
     vendor/samsung/klte-common/proprietary/vendor/lib/C16QL_libchromatix_s5k2p2xx_common.so:system/vendor/lib/C16QL_libchromatix_s5k2p2xx_common.so \
     vendor/samsung/klte-common/proprietary/vendor/lib/C16QL_libchromatix_s5k2p2xx_common_res0.so:system/vendor/lib/C16QL_libchromatix_s5k2p2xx_common_res0.so \
