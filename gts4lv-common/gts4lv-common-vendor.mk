2af7ba1d8e4a553f4aebe1d88bf9ae45eb6c65bf
Bruno Martins <bgcngm@gmail.com>
2020-10-31 23:07:33 +0000

    gts4lv-common: Decommonize ACDB data and WiFi firmware blobs
    These are device-specific. The actual ones belong to the wifi variant,
    so move them there.
    
    Change-Id: I36149d3eb515f8b511072b439d8d85f172b5377d

--- gts4lv-common/gts4lv-common-vendor.mk
+++ gts4lv-common/gts4lv-common-vendor.mk
@@ -104,30 +104,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/gts4lv-common/proprietary/vendor/bin/vppservice:$(TARGET_COPY_OUT_VENDOR)/bin/vppservice \
     vendor/samsung/gts4lv-common/proprietary/vendor/bin/wifidisplayhalservice:$(TARGET_COPY_OUT_VENDOR)/bin/wifidisplayhalservice \
     vendor/samsung/gts4lv-common/proprietary/vendor/bin/xtra-daemon:$(TARGET_COPY_OUT_VENDOR)/bin/xtra-daemon \
-    vendor/samsung/gts4lv-common/proprietary/vendor/etc/audconf/ODM/Bluetooth_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/ODM/Bluetooth_cal.acdb \
-    vendor/samsung/gts4lv-common/proprietary/vendor/etc/audconf/ODM/General_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/ODM/General_cal.acdb \
-    vendor/samsung/gts4lv-common/proprietary/vendor/etc/audconf/ODM/Global_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/ODM/Global_cal.acdb \
-    vendor/samsung/gts4lv-common/proprietary/vendor/etc/audconf/ODM/Handset_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/ODM/Handset_cal.acdb \
-    vendor/samsung/gts4lv-common/proprietary/vendor/etc/audconf/ODM/Hdmi_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/ODM/Hdmi_cal.acdb \
-    vendor/samsung/gts4lv-common/proprietary/vendor/etc/audconf/ODM/Headset_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/ODM/Headset_cal.acdb \
-    vendor/samsung/gts4lv-common/proprietary/vendor/etc/audconf/ODM/Speaker_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/ODM/Speaker_cal.acdb \
-    vendor/samsung/gts4lv-common/proprietary/vendor/etc/audconf/ODM/workspaceFile.qwsp:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/ODM/workspaceFile.qwsp \
-    vendor/samsung/gts4lv-common/proprietary/vendor/etc/audconf/OLM/Bluetooth_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/OLM/Bluetooth_cal.acdb \
-    vendor/samsung/gts4lv-common/proprietary/vendor/etc/audconf/OLM/General_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/OLM/General_cal.acdb \
-    vendor/samsung/gts4lv-common/proprietary/vendor/etc/audconf/OLM/Global_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/OLM/Global_cal.acdb \
-    vendor/samsung/gts4lv-common/proprietary/vendor/etc/audconf/OLM/Handset_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/OLM/Handset_cal.acdb \
-    vendor/samsung/gts4lv-common/proprietary/vendor/etc/audconf/OLM/Hdmi_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/OLM/Hdmi_cal.acdb \
-    vendor/samsung/gts4lv-common/proprietary/vendor/etc/audconf/OLM/Headset_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/OLM/Headset_cal.acdb \
-    vendor/samsung/gts4lv-common/proprietary/vendor/etc/audconf/OLM/Speaker_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/OLM/Speaker_cal.acdb \
-    vendor/samsung/gts4lv-common/proprietary/vendor/etc/audconf/OLM/workspaceFile.qwsp:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/OLM/workspaceFile.qwsp \
-    vendor/samsung/gts4lv-common/proprietary/vendor/etc/audconf/OPEN/Bluetooth_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/OPEN/Bluetooth_cal.acdb \
-    vendor/samsung/gts4lv-common/proprietary/vendor/etc/audconf/OPEN/General_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/OPEN/General_cal.acdb \
-    vendor/samsung/gts4lv-common/proprietary/vendor/etc/audconf/OPEN/Global_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/OPEN/Global_cal.acdb \
-    vendor/samsung/gts4lv-common/proprietary/vendor/etc/audconf/OPEN/Handset_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/OPEN/Handset_cal.acdb \
-    vendor/samsung/gts4lv-common/proprietary/vendor/etc/audconf/OPEN/Hdmi_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/OPEN/Hdmi_cal.acdb \
-    vendor/samsung/gts4lv-common/proprietary/vendor/etc/audconf/OPEN/Headset_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/OPEN/Headset_cal.acdb \
-    vendor/samsung/gts4lv-common/proprietary/vendor/etc/audconf/OPEN/Speaker_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/OPEN/Speaker_cal.acdb \
-    vendor/samsung/gts4lv-common/proprietary/vendor/etc/audconf/OPEN/workspaceFile.qwsp:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/OPEN/workspaceFile.qwsp \
     vendor/samsung/gts4lv-common/proprietary/vendor/etc/cacert_location.pem:$(TARGET_COPY_OUT_VENDOR)/etc/cacert_location.pem \
     vendor/samsung/gts4lv-common/proprietary/vendor/etc/configstore/vpp.configstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/configstore/vpp.configstore.xml \
     vendor/samsung/gts4lv-common/proprietary/vendor/etc/data/dsi_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/data/dsi_config.xml \
@@ -207,11 +183,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/gts4lv-common/proprietary/vendor/firmware/dbmd8_va_fw.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/dbmd8_va_fw.bin \
     vendor/samsung/gts4lv-common/proprietary/vendor/firmware/leia_pfp_470.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/leia_pfp_470.fw \
     vendor/samsung/gts4lv-common/proprietary/vendor/firmware/leia_pm4_470.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/leia_pm4_470.fw \
-    vendor/samsung/gts4lv-common/proprietary/vendor/firmware/wlan/qca_cld/bdwlan.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/qca_cld/bdwlan.bin \
-    vendor/samsung/gts4lv-common/proprietary/vendor/firmware/wlan/qca_cld/bdwlan.bin_FEM:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/qca_cld/bdwlan.bin_FEM \
-    vendor/samsung/gts4lv-common/proprietary/vendor/firmware/wlan/qca_cld/grippower.info:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/qca_cld/grippower.info \
-    vendor/samsung/gts4lv-common/proprietary/vendor/firmware/wlan/qca_cld/regdb.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/qca_cld/regdb.bin \
-    vendor/samsung/gts4lv-common/proprietary/vendor/firmware/wlanmdsp.mbn:$(TARGET_COPY_OUT_VENDOR)/firmware/wlanmdsp.mbn \
     vendor/samsung/gts4lv-common/proprietary/vendor/lib/android.hardware.camera.provider@2.4-legacy.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.hardware.camera.provider@2.4-legacy.so \
     vendor/samsung/gts4lv-common/proprietary/vendor/lib/android.hardware.camera.provider@2.5-legacy.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.hardware.camera.provider@2.5-legacy.so \
     vendor/samsung/gts4lv-common/proprietary/vendor/lib/camera.device@1.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera.device@1.0-impl.so \
