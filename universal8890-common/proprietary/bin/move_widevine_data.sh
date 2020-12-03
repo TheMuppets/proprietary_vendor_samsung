b0b61df45795f6dc0f0a7422184e3dc5e8238c1c
Francescodario Cuzzocrea <bosconovic@gmail.com>
2020-12-03 21:56:21 +0100

    universal8890:  update blobs from N935FXXS7CTJ2
     * also move them to vendor while we are at it

--- universal8890-common/proprietary/bin/move_widevine_data.sh
+++ universal8890-common/proprietary/bin/move_widevine_data.sh
@@ -3,7 +3,6 @@
 DEST_PATH="/data/vendor/mediadrm"
 FILES_MOVED="/data/vendor/mediadrm/files_moved"
 SRC_PATH="/data/mediadrm"
-L3_FILE_PATTERN="ay64.dat*"
 
 if [ ! -f "$FILES_MOVED" ]; then
   for i in "$SRC_PATH/IDM"*; do
@@ -11,13 +10,6 @@ if [ ! -f "$FILES_MOVED" ]; then
     if [ -d "$i" ]; then
       mkdir -p $dest_path -m 700
       mv $i "$DEST_PATH"
-      for file in ${dest_path}/${L3_FILE_PATTERN}; do
-        [ -e "${file}" ] || continue
-        # We need to move any L3 files in the IDM folder to the L3 subfolder.
-        l3_dir="${dest_path}/L3/"
-        mkdir -p "${l3_dir}" -m 700
-        mv "${file}" "${l3_dir}"
-      done
       find $dest_path -print0 | while IFS= read -r -d '' file
         do
           chgrp media "$file"
