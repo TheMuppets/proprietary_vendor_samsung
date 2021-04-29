a4430f4ffdf79c1b2a033360ed217a52c3a7cf76
Francescodario Cuzzocrea <bosconovic@gmail.com>
2021-04-29 10:08:58 +0200

    universal8890: restore vulkan hal symlink
        * also move it out the hw/ folder

--- universal8890-common/Android.mk
+++ universal8890-common/Android.mk
@@ -8,7 +8,7 @@ ifneq ($(filter herolte hero2lte,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 
-EGL_LIBS := libGLES_mali.so libOpenCL.so libOpenCL.so.1 libOpenCL.so.1.1
+EGL_LIBS := libGLES_mali.so libOpenCL.so libOpenCL.so.1 libOpenCL.so.1.1 vulkan.exynos5.so
 
 EGL_32_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib/,$(EGL_LIBS))
 $(EGL_32_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
@@ -24,22 +24,6 @@ $(EGL_64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@rm -rf $@
 	$(hide) ln -sf /vendor/lib64/egl/libGLES_mali.so $@
 
-VULKAN_LIBS := vulkan.exynos5.so
-
-VULKAN_32_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib/hw/,$(VULKAN_LIBS))
-$(VULKAN_32_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "Copy: Vulkan 32-bit lib: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) cp $(TARGET_OUT_VENDOR)/lib/egl/libGLES_mali.so $@
-
-VULKAN_64_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib64/hw/,$(VULKAN_LIBS))
-$(VULKAN_64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "Copy: Vulkan 64-bit lib: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) cp $(TARGET_OUT_VENDOR)/lib64/egl/libGLES_mali.so $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(EGL_32_SYMLINKS) $(EGL_64_SYMLINKS) $(VULKAN_32_SYMLINKS) $(VULKAN_64_SYMLINKS)
+ALL_DEFAULT_INSTALLED_MODULES += $(EGL_32_SYMLINKS) $(EGL_64_SYMLINKS)
 
 endif
