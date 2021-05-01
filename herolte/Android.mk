fe3e0beb6db1235f56b794dfc1be2c9d0699a6fb
Francescodario Cuzzocrea <bosconovic@gmail.com>
2021-05-01 10:53:06 +0200

    hero{2}lte: import lifevibes blobs

--- herolte/Android.mk
+++ herolte/Android.mk
@@ -6,4 +6,17 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),herolte)
 
+include $(CLEAR_VARS)
+
+LIFEVIBES_LIBS := libLifevibes_lvverx.so libLifevibes_lvvetx.so
+
+LIFEVIBES_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib/,$(notdir $(LIFEVIBES_LIBS)))
+$(LIFEVIBES_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "LifeVibes lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /vendor/lib/soundfx/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(LIFEVIBES_SYMLINKS)
+
 endif
