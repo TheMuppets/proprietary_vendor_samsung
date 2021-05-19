332fcf062d447c4cee54ccd240553f310d793f1c
Arne Coucheron <arco68@gmail.com>
2021-05-20 00:53:10 +0200

    serrano: Regenerate scripts

--- serrano-common/Android.mk
+++ serrano-common/Android.mk
@@ -1,5 +1,5 @@
 # Copyright (C) 2013-2016 The CyanogenMod Project
-# Copyright (C) 2017-2019 The LineageOS Project
+# Copyright (C) 2017-2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
