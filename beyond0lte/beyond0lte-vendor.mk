18af47d28e1773fe9c8ace235b0da734b766615b
Tim Zimmermann <tim@linux4.de>
2021-06-22 18:03:13 +0200

    beyond0lte: decommonize some blobs needed for note 10 bringup
    Change-Id: I73083b7aef5c435a8a2c6689d7700a4184d0fa57

--- beyond0lte/beyond0lte-vendor.mk
+++ beyond0lte/beyond0lte-vendor.mk
@@ -6,12 +6,21 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/samsung/beyond0lte
 
 PRODUCT_COPY_FILES += \
+    vendor/samsung/beyond0lte/proprietary/vendor/bin/hw/gpsd:$(TARGET_COPY_OUT_VENDOR)/bin/hw/gpsd \
+    vendor/samsung/beyond0lte/proprietary/vendor/bin/hw/lhd:$(TARGET_COPY_OUT_VENDOR)/bin/hw/lhd \
     vendor/samsung/beyond0lte/proprietary/vendor/bin/secril_config_svc:$(TARGET_COPY_OUT_VENDOR)/bin/secril_config_svc \
     vendor/samsung/beyond0lte/proprietary/vendor/etc/floating_feature.xml:$(TARGET_COPY_OUT_VENDOR)/etc/floating_feature.xml \
+    vendor/samsung/beyond0lte/proprietary/vendor/etc/gnss/gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.xml \
     vendor/samsung/beyond0lte/proprietary/vendor/etc/init/init.baseband.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.baseband.rc \
+    vendor/samsung/beyond0lte/proprietary/vendor/etc/init/init.gps.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.gps.rc \
+    vendor/samsung/beyond0lte/proprietary/vendor/etc/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     vendor/samsung/beyond0lte/proprietary/vendor/etc/mixer_gains.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_gains.xml \
+    vendor/samsung/beyond0lte/proprietary/vendor/etc/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
+    vendor/samsung/beyond0lte/proprietary/vendor/etc/plmn_se13.bin:$(TARGET_COPY_OUT_VENDOR)/etc/plmn_se13.bin \
+    vendor/samsung/beyond0lte/proprietary/vendor/etc/sensor/lhd.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensor/lhd.conf \
     vendor/samsung/beyond0lte/proprietary/vendor/firmware/APBargeIn_AUDIO_SLSI.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/APBargeIn_AUDIO_SLSI.bin \
     vendor/samsung/beyond0lte/proprietary/vendor/firmware/APBiBF_AUDIO_SLSI.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/APBiBF_AUDIO_SLSI.bin \
+    vendor/samsung/beyond0lte/proprietary/vendor/firmware/APDVTX_AUDIO_SLSI.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/APDVTX_AUDIO_SLSI.bin \
     vendor/samsung/beyond0lte/proprietary/vendor/firmware/APDV_AUDIO_SLSI.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/APDV_AUDIO_SLSI.bin \
     vendor/samsung/beyond0lte/proprietary/vendor/firmware/APSV_AUDIO_SLSI.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/APSV_AUDIO_SLSI.bin \
     vendor/samsung/beyond0lte/proprietary/vendor/firmware/AP_AUDIO_SLSI.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/AP_AUDIO_SLSI.bin \
@@ -22,20 +31,53 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/beyond0lte/proprietary/vendor/firmware/bcm4375B1_semco.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/bcm4375B1_semco.hcd \
     vendor/samsung/beyond0lte/proprietary/vendor/firmware/bcm4375B1_semco_sem.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/bcm4375B1_semco_sem.hcd \
     vendor/samsung/beyond0lte/proprietary/vendor/firmware/bcmdhd_clm.blob:$(TARGET_COPY_OUT_VENDOR)/firmware/bcmdhd_clm.blob \
+    vendor/samsung/beyond0lte/proprietary/vendor/firmware/bidirmic.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/bidirmic.bin \
+    vendor/samsung/beyond0lte/proprietary/vendor/firmware/calliope2.dt:$(TARGET_COPY_OUT_VENDOR)/firmware/calliope2.dt \
+    vendor/samsung/beyond0lte/proprietary/vendor/firmware/calliope_dram.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/calliope_dram.bin \
+    vendor/samsung/beyond0lte/proprietary/vendor/firmware/calliope_dram_2.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/calliope_dram_2.bin \
+    vendor/samsung/beyond0lte/proprietary/vendor/firmware/calliope_sram.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/calliope_sram.bin \
+    vendor/samsung/beyond0lte/proprietary/vendor/firmware/calliope_sram_2.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/calliope_sram_2.bin \
     vendor/samsung/beyond0lte/proprietary/vendor/firmware/cs35l40-bot-dsp1-spk-prot-calib.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/cs35l40-bot-dsp1-spk-prot-calib.bin \
     vendor/samsung/beyond0lte/proprietary/vendor/firmware/cs35l40-bot-dsp1-spk-prot.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/cs35l40-bot-dsp1-spk-prot.bin \
+    vendor/samsung/beyond0lte/proprietary/vendor/firmware/cs35l40-bot-dsp1-spk-prot.wmfw:$(TARGET_COPY_OUT_VENDOR)/firmware/cs35l40-bot-dsp1-spk-prot.wmfw \
     vendor/samsung/beyond0lte/proprietary/vendor/firmware/cs35l40-rcv-dsp1-spk-prot-calib.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/cs35l40-rcv-dsp1-spk-prot-calib.bin \
     vendor/samsung/beyond0lte/proprietary/vendor/firmware/cs35l40-rcv-dsp1-spk-prot.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/cs35l40-rcv-dsp1-spk-prot.bin \
+    vendor/samsung/beyond0lte/proprietary/vendor/firmware/cs35l40-rcv-dsp1-spk-prot.wmfw:$(TARGET_COPY_OUT_VENDOR)/firmware/cs35l40-rcv-dsp1-spk-prot.wmfw \
+    vendor/samsung/beyond0lte/proprietary/vendor/firmware/dax_param.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/dax_param.bin \
+    vendor/samsung/beyond0lte/proprietary/vendor/firmware/dnn.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/dnn.bin \
+    vendor/samsung/beyond0lte/proprietary/vendor/firmware/dual_cal_wide_swide.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/dual_cal_wide_swide.bin \
+    vendor/samsung/beyond0lte/proprietary/vendor/firmware/dual_cal_wide_tele.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/dual_cal_wide_tele.bin \
     vendor/samsung/beyond0lte/proprietary/vendor/firmware/fimc_is_fw_mcu.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/fimc_is_fw_mcu.bin \
     vendor/samsung/beyond0lte/proprietary/vendor/firmware/fimc_is_lib.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/fimc_is_lib.bin \
     vendor/samsung/beyond0lte/proprietary/vendor/firmware/fimc_is_rta.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/fimc_is_rta.bin \
+    vendor/samsung/beyond0lte/proprietary/vendor/firmware/fmsb.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/fmsb.bin \
+    vendor/samsung/beyond0lte/proprietary/vendor/firmware/nfc/sec_s3nrn82_firmware.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nfc/sec_s3nrn82_firmware.bin \
+    vendor/samsung/beyond0lte/proprietary/vendor/firmware/nvram.txt_1rh_es11_b0:$(TARGET_COPY_OUT_VENDOR)/firmware/nvram.txt_1rh_es11_b0 \
+    vendor/samsung/beyond0lte/proprietary/vendor/firmware/nvram.txt_1rh_es12_b0:$(TARGET_COPY_OUT_VENDOR)/firmware/nvram.txt_1rh_es12_b0 \
+    vendor/samsung/beyond0lte/proprietary/vendor/firmware/nvram.txt_1rh_es13_b0:$(TARGET_COPY_OUT_VENDOR)/firmware/nvram.txt_1rh_es13_b0 \
     vendor/samsung/beyond0lte/proprietary/vendor/firmware/nvram.txt_1rh_es43_b1:$(TARGET_COPY_OUT_VENDOR)/firmware/nvram.txt_1rh_es43_b1 \
     vendor/samsung/beyond0lte/proprietary/vendor/firmware/nvram.txt_CS01_semco_b1:$(TARGET_COPY_OUT_VENDOR)/firmware/nvram.txt_CS01_semco_b1 \
     vendor/samsung/beyond0lte/proprietary/vendor/firmware/nvram.txt_ES03_semco_b0:$(TARGET_COPY_OUT_VENDOR)/firmware/nvram.txt_ES03_semco_b0 \
+    vendor/samsung/beyond0lte/proprietary/vendor/firmware/rxse.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/rxse.bin \
     vendor/samsung/beyond0lte/proprietary/vendor/firmware/setfile_2l4.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/setfile_2l4.bin \
+    vendor/samsung/beyond0lte/proprietary/vendor/firmware/setfile_3j1.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/setfile_3j1.bin \
     vendor/samsung/beyond0lte/proprietary/vendor/firmware/setfile_3m3.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/setfile_3m3.bin \
     vendor/samsung/beyond0lte/proprietary/vendor/firmware/setfile_3p9.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/setfile_3p9.bin \
+    vendor/samsung/beyond0lte/proprietary/vendor/firmware/setfile_4ha.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/setfile_4ha.bin \
+    vendor/samsung/beyond0lte/proprietary/vendor/firmware/txse1.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/txse1.bin \
+    vendor/samsung/beyond0lte/proprietary/vendor/firmware/txse2.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/txse2.bin \
+    vendor/samsung/beyond0lte/proprietary/vendor/firmware/vts.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/vts.bin \
+    vendor/samsung/beyond0lte/proprietary/vendor/lib/hw/audio.primary.exynos9820.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/audio.primary.exynos9820.so \
     vendor/samsung/beyond0lte/proprietary/vendor/lib/hw/camera.exynos9820.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/camera.exynos9820.so \
+    vendor/samsung/beyond0lte/proprietary/vendor/lib/hw/gatekeeper.exynos9820.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/gatekeeper.exynos9820.so \
+    vendor/samsung/beyond0lte/proprietary/vendor/lib/hw/gralloc.exynos9820.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/gralloc.exynos9820.so \
+    vendor/samsung/beyond0lte/proprietary/vendor/lib/hw/hwcomposer.exynos9820.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/hwcomposer.exynos9820.so \
+    vendor/samsung/beyond0lte/proprietary/vendor/lib/hw/memtrack.exynos9820.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/memtrack.exynos9820.so \
+    vendor/samsung/beyond0lte/proprietary/vendor/lib/hw/sensors.exynos9820.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/sensors.exynos9820.so \
+    vendor/samsung/beyond0lte/proprietary/vendor/lib/hw/sound_trigger.primary.exynos9820.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/sound_trigger.primary.exynos9820.so \
+    vendor/samsung/beyond0lte/proprietary/vendor/lib/lib_SamsungRec_07010.so:$(TARGET_COPY_OUT_VENDOR)/lib/lib_SamsungRec_07010.so \
+    vendor/samsung/beyond0lte/proprietary/vendor/lib/libaudio_soundtrigger.so:$(TARGET_COPY_OUT_VENDOR)/lib/libaudio_soundtrigger.so \
+    vendor/samsung/beyond0lte/proprietary/vendor/lib/libaudioproxy.so:$(TARGET_COPY_OUT_VENDOR)/lib/libaudioproxy.so \
     vendor/samsung/beyond0lte/proprietary/vendor/lib/libbauthserver.so:$(TARGET_COPY_OUT_VENDOR)/lib/libbauthserver.so \
     vendor/samsung/beyond0lte/proprietary/vendor/lib/libbauthtzcommon.so:$(TARGET_COPY_OUT_VENDOR)/lib/libbauthtzcommon.so \
     vendor/samsung/beyond0lte/proprietary/vendor/lib/libexynoscamera3.so:$(TARGET_COPY_OUT_VENDOR)/lib/libexynoscamera3.so \
@@ -45,7 +87,16 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/beyond0lte/proprietary/vendor/lib/libsec-ril.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsec-ril.so \
     vendor/samsung/beyond0lte/proprietary/vendor/lib/sensors.bio.so:$(TARGET_COPY_OUT_VENDOR)/lib/sensors.bio.so \
     vendor/samsung/beyond0lte/proprietary/vendor/lib/sensors.sensorhub.so:$(TARGET_COPY_OUT_VENDOR)/lib/sensors.sensorhub.so \
+    vendor/samsung/beyond0lte/proprietary/vendor/lib/soundfx/libaudioeffectoffload.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libaudioeffectoffload.so \
+    vendor/samsung/beyond0lte/proprietary/vendor/lib64/hw/android.hardware.gnss@2.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/android.hardware.gnss@2.0-impl.so \
     vendor/samsung/beyond0lte/proprietary/vendor/lib64/hw/camera.exynos9820.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/camera.exynos9820.so \
+    vendor/samsung/beyond0lte/proprietary/vendor/lib64/hw/gatekeeper.exynos9820.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/gatekeeper.exynos9820.so \
+    vendor/samsung/beyond0lte/proprietary/vendor/lib64/hw/gps.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/gps.default.so \
+    vendor/samsung/beyond0lte/proprietary/vendor/lib64/hw/gralloc.exynos9820.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/gralloc.exynos9820.so \
+    vendor/samsung/beyond0lte/proprietary/vendor/lib64/hw/hwcomposer.exynos9820.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/hwcomposer.exynos9820.so \
+    vendor/samsung/beyond0lte/proprietary/vendor/lib64/hw/memtrack.exynos9820.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/memtrack.exynos9820.so \
+    vendor/samsung/beyond0lte/proprietary/vendor/lib64/hw/sensors.exynos9820.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/sensors.exynos9820.so \
+    vendor/samsung/beyond0lte/proprietary/vendor/lib64/hw/vendor.samsung.hardware.gnss@2.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/vendor.samsung.hardware.gnss@2.0-impl.so \
     vendor/samsung/beyond0lte/proprietary/vendor/lib64/libbauthserver.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libbauthserver.so \
     vendor/samsung/beyond0lte/proprietary/vendor/lib64/libbauthtzcommon.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libbauthtzcommon.so \
     vendor/samsung/beyond0lte/proprietary/vendor/lib64/libexynoscamera3.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libexynoscamera3.so \
@@ -55,6 +106,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/beyond0lte/proprietary/vendor/lib64/libwrappergps.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libwrappergps.so \
     vendor/samsung/beyond0lte/proprietary/vendor/lib64/sensors.bio.so:$(TARGET_COPY_OUT_VENDOR)/lib64/sensors.bio.so \
     vendor/samsung/beyond0lte/proprietary/vendor/lib64/sensors.sensorhub.so:$(TARGET_COPY_OUT_VENDOR)/lib64/sensors.sensorhub.so \
+    vendor/samsung/beyond0lte/proprietary/vendor/lib64/soundfx/libaudioeffectoffload.so:$(TARGET_COPY_OUT_VENDOR)/lib64/soundfx/libaudioeffectoffload.so \
     vendor/samsung/beyond0lte/proprietary/vendor/tee/00000000-0000-0000-0000-00000000dead:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-00000000dead \
     vendor/samsung/beyond0lte/proprietary/vendor/tee/00000000-0000-0000-0000-000000534b4d:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-000000534b4d \
     vendor/samsung/beyond0lte/proprietary/vendor/tee/00000000-0000-0000-0000-0053545354ab:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-0053545354ab \
